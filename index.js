ce04bda536c3981407cacdc93e6b18f9b98a10c2
ChiendeGlace <szymon.szostak72@gmail.com>
2023-01-07 14:32:42 +0100

    Add reconstruct path function

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 const buildGraph = (graph, a, b) => {
     if (!(a in graph)) graph[a] = [];
     if (!(b in graph)) graph[b] = [];
-    graph[a].push(b);
     graph[b].push(a);
     return graph;
+};
+
+const checkIfVisited = (visited, neighbour) => {
+    for (let i = 0; i < visited.length; i++) {
+        if (visited[i][0] === neighbour[0] && visited[i][1] === neighbour[1]) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const solve = (graph, s) => {
+    const queue = [s];
+    const visited = [s];
+    const prev = [];
+    while (queue.length > 0) {
+        const current = queue.shift();
+        for (let neighbour of graph[current]) {
+            if (checkIfVisited(visited, neighbour) == false) {
+                queue.push(neighbour);
+                visited.push(neighbour);
+                prev[neighbour] = current;
+            }
+        }
+    }
+    return prev;
+};
+
+const reconstructPath = (s, e, prev) => {
+    let path = [];
+    debugger;
+    for (let i = e; i != null; i = prev[i]) {
+        path.push(i);
+    }
+    const reversedPath = [];
+    for (let i = path.length - 1; i >= 0; i--) {
+        reversedPath.push(path[i]);
+    }
+    if (path[path.length - 1] == s) {
+        return reversedPath;
+    }
+    return;
 };
 
 const knightMoves = (start, end) => {
@@ -35,8 +76,12 @@ const knightMoves = (start, end) => {
             }
         }
     }
-    return graph;
-
+    let prev = solve(graph, start);
+    let result = reconstructPath(start, end, prev);
+    let distance = result.length - 1;
+    console.log(`Distance from [${start}] to [${end}] is ${distance}.`);
+    console.log(`And the path was:`);
+    console.log(...result);
 };
 
-console.log(knightMoves([1, 2], [6, 7]));
+knightMoves([6, 1], [0, 0]);
